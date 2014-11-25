6b2d19857ac65bdcf8617988b174ab82b797c6e8
Michael Alexander <beefsack@gmail.com>
2014-11-25 23:22:57 +1100

    Game display.

--- js/app.js
+++ js/app.js
@@ -10,28 +10,49 @@ App.Router.map(function() {
 
 App.GamesIndexRoute = Ember.Route.extend({
 	model: function(params) {
-		return [{
-			name: "Roll Through The Ages",
-			id: "1234-5678",
-			players: ["beefsack@gmail.com", "baconheist@gmail.com"]
-		}, {
-			name: "Acquire",
-			id: "1235-567878",
-			players: ["beefsack@gmail.com", "baconheist@gmail.com"]
-		}];
+		return this.store.find('game', {
+			gameState: 'active'
+		});
 	}
 });
 
 App.GamesShowRoute = Ember.Route.extend({
+	loadedFresh: false,
+	beforeModel: function() {
+		this.loadedFresh = false;
+	},
 	model: function(params) {
-		return {
-			name: "Roll Through The Ages",
-			id: params.id,
-			players: ["beefsack@gmail.com", "baconheist@gmail.com"]
-		};
+		this.loadedFresh = true;
+		return this.store.find('game', params.id);
+	},
+	afterModel: function(model) {
+		// If we didn't hit the DB, force a reload.
+		if (!this.loadedFresh) {
+			return model.reload();
+		}
 	}
 });
+App.GamesShowController = Ember.ObjectController.extend({
+	command: '',
+	actions: {
+		sendCommand: function() {
+			toastr.info('sending command ' + this.get('command'));
+			this.set('command', '');
+			this.get('model').reload();
+		}
+	},
+	// Scroll log when new logs come in
+	logChanged: function() {
+		if (this.get('model.log')) {
+			Ember.run.scheduleOnce('afterRender', this, function() {
+				var logDiv = $('.game-log')[0];
+				logDiv.scrollTop = logDiv.scrollHeight;
+			});
+		}
+	}.observes('model.log')
+});
 
+// Application controller handles auth
 App.ApplicationController = Ember.Controller.extend({
 	authToken: localStorage.getItem('token'),
 	authEmail: '',
@@ -39,6 +60,7 @@ App.ApplicationController = Ember.Controller.extend({
 	requesting: false,
 	actions: {
 		requestAuth: function() {
+			var controller = this;
 			this.set('requesting', true);
 			$.ajax('http://brdg.me/auth/request', {
 				type: 'POST',
@@ -46,14 +68,14 @@ App.ApplicationController = Ember.Controller.extend({
 					email: this.get('authEmail')
 				},
 				error: function() {
-					self.set('requesting', false);
+					controller.set('requesting', false);
 					alert('Unable to request auth, please ensure you have entered a valid email address.');
 				}
 			});
 			alert('Please check your email for a confirmation code.');
 		},
 		confirmAuth: function() {
-			var self = this;
+			var controller = this;
 			$.ajax('http://brdg.me/auth/confirm', {
 				type: 'POST',
 				data: {
@@ -62,14 +84,46 @@ App.ApplicationController = Ember.Controller.extend({
 				},
 				success: function(data) {
 					alert('You logged in and your token is ' + data);
-					self.set('authToken', data);
+					controller.set('authToken', data);
 					localStorage.setItem('token', data);
 				},
 				error: function() {
-					self.set('requesting', false);
+					controller.set('requesting', false);
 					alert('Unable to confirm, please check the email and confirmation code is correct.');
 				}
 			});
 		}
 	}
+});
+
+// Data / API stuff
+App.Game = DS.Model.extend({
+	commands: DS.attr(),
+	game: DS.attr(),
+	log: DS.attr(),
+	identifier: DS.attr(),
+	isFinished: DS.attr(),
+	name: DS.attr(),
+	playerList: DS.attr(),
+	whoseTurn: DS.attr(),
+	winners: DS.attr()
+});
+App.GameSerializer = DS.RESTSerializer.extend({
+	normalizePayload: function(payload) {
+		return {
+			game: payload
+		};
+	}
+});
+App.ApplicationAdapter = DS.RESTAdapter.extend({
+	pathForType: function(type) {
+		// Disable pluralisation
+		return type;
+	},
+	headers: function() {
+		return {
+			Authorization: 'token ' + localStorage.getItem('token')
+		};
+	}.property().volatile(),
+	host: 'http://brdg.me'
 });
